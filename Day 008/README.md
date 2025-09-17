648ee36164c3f3852de6b8990438d157f5790e3c
Batuhan Eray Aktayca <batuhanea@gmail.com>
2025-09-17 03:04:50 +0300

    Day 008 of the course, learned to commit to github through VSCode

---
+++ Day 008/README.md
@@ -0,0 +1,7 @@
+# Day 008
+
+## Caesar Cipher
+A program that encodes and decodes text using the Caesar Cipher technique.
+Built to practice functions, parameters, loops, and string manipulation.
+
+![Caesar Cipher Screenshot](images\caesarCipher.png)
---
+++ Day 008/caesarCipher.py
@@ -0,0 +1,27 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for char in original_text:
+        if char in alphabet:
+            shifted_position = alphabet.index(char) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += char
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+from art import logo
+cycle= 'y'
+while cycle == 'y':
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    cycle = input("Type 'y' if you want to use the cipher again.")
