e963433c842610f8d25f89a938e3bfaaf055dee2
Batuhan Eray Aktayca <batuhanea@gmail.com>
2025-10-15 06:51:51 +0300

    error handling for non-alphabetic inputs

--- Day 026/main.py
+++ Day 026/main.py
@@ -7,13 +7,18 @@ nato_dict = {row["letter"]:row["code"] for (index, row) in df.iterrows()}
 again = True
 
 while again:
-    word = input("\nWhat do you want to spell in NATO alphabet? ")
-    word_upper = word.upper()
-    letter_list = [letter for letter in word_upper]
-    result = [nato_dict[letter] for letter in letter_list]
-    print(f"You can spell {word} as {result} in the NATO Phonetic Alphabet.")
-    i = input(f"\nDo you want to convert another word? Y/N  ").upper()
-    if i == "Y":
-        continue
-    else:
-        again = False
+        word = input("\nWhat do you want to spell in NATO alphabet? ")
+        word_upper = word.upper()
+        letter_list = [letter for letter in word_upper]
+        try:
+            result = [nato_dict[letter] for letter in letter_list]
+        except KeyError:
+            print("Only letters in the alphabet please!")
+        else:
+            print(f"You can spell {word} as {result} in the NATO Phonetic Alphabet.")
+        finally:
+            i = input(f"\nDo you want to convert another word? Y/N  ").upper()
+            if i == "Y":
+                continue
+            else:
+                    again = False
